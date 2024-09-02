58d3be7668a1a88d336e47eba4606e2d021f6469
acmicpc0614 <PleaseBugMeNot.dev@gmail.com>
2024-09-01 19:17:08 -0500

    [pref] article Item style

--- src/components/ArticleItem.js
+++ src/components/ArticleItem.js
@@ -1,4 +1,9 @@
-const ArticleItem = () => {
-  return <></>;
+const ArticleItem = ({ title, description }) => {
+  return (
+    <div className="w-full h-28 rounded-sm">
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </div>
+  );
 };
 export default ArticleItem;
